8cd464e98cb24ea11bb362d8e8bf95fdc4f499ad
Elliot <Wolfy858@gmail.com>
2015-12-04 12:52:07 -0800

    create user model, write getters/setters for password digest

---
+++ app/models/user.rb
@@ -0,0 +1,22 @@
+class User < ActiveRecord::Base
+
+  attr_reader :password
+
+  validates :username, :password_digest, :session_token, presence: true
+  validates :password, length: { minimum: 6, allow_nil: true }
+
+  def password=(password)
+    @password = password
+    self.password_digest = BCrypt::Password.create(password)
+  end
+
+  # def is_password?(password)
+  #   BCrypt::Password.new(self.password_digest).is_password?(password)
+  # end
+
+  def password_digest
+    BCrypt::Password.new(super)
+  end
+
+
+end
