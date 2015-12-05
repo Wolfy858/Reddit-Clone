4077f5a0810738b406ba73cc61f5d6b51a429bc7
Elliot <Wolfy858@gmail.com>
2015-12-04 18:14:38 -0800

    add methods and callback for establishing session token to User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,8 @@ class User < ActiveRecord::Base
   validates :password, length: { minimum: 6, allow_nil: true }
   validates :username, uniqueness: true
 
+  after_initialize :ensure_session_token
+
   def self.find_by_credentials(username, password)
     user = User.find_by_username(username)
     return nil if user.nil?
@@ -21,5 +23,20 @@ class User < ActiveRecord::Base
     BCrypt::Password.new(super)
   end
 
+  def self.generate_session_token
+    SecureRandom::urlsafe_base64(16)
+  end
+
+  def reset_session_token!
+    self.session_token = self.class.generate_session_token
+    self.save!
+    self.session_token
+  end
+
+  private
+
+  def ensure_session_token
+    self.session_token ||= self.class.generate_session_token
+  end
 
 end
