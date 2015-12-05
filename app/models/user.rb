79bcdaea46fa569ec0af473a0689527f3b2a8fea
Elliot <Wolfy858@gmail.com>
2015-12-04 16:16:01 -0800

    add routes for User, write create and new methods for UsersController

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,10 +10,6 @@ class User < ActiveRecord::Base
     self.password_digest = BCrypt::Password.create(password)
   end
 
-  # def is_password?(password)
-  #   BCrypt::Password.new(self.password_digest).is_password?(password)
-  # end
-
   def password_digest
     BCrypt::Password.new(super)
   end
