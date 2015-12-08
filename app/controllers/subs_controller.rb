22f2448c800a1d8390993915fe5195987387ca19
Elliot <Wolfy858@gmail.com>
2015-12-07 20:15:30 -0800

    finish writing initial subcontroller methods, create views for sub /new and /edit

--- app/controllers/subs_controller.rb
+++ app/controllers/subs_controller.rb
@@ -1,9 +1,45 @@
 class SubsControllers < ApplicationController
 
+  def index
+  end
+
+  def show
+  end
+
   def new
     @sub = Sub.new
     render :new
   end
 
+  def create
+    @sub = Sub.new(sub_params)
+    if @sub.save
+      redirect_to sub_url
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @sub = Sub.find(params[:id])
+    render :edit
+  end
+
+  def update
+    @sub = Sub.find(params[:id])
+    if @sub.save
+      redirect_to sub_url
+    else
+      render :edit
+    end
+  end
+
+
+
+  private
+
+  def sub_params
+    params.require(:sub).permit(:name, :description, :moderator)
+  end
 
 end
